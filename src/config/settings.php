06b9397c41e64f92c8b5c3479ea8e35260fd2a1e
Gregor Kralik <g.kralik@gmail.com>
2017-04-21 20:24:35 +0200

    remove logger and renderer from container

--- src/config/settings.php
+++ src/config/settings.php
@@ -7,4 +7,12 @@ return [
         'displayErrorDetails' => !$isProductionEnvironment, // do not display error details in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
     ],
+    'db' => [
+        'driver'   => 'pdo_mysql',
+        'host'     => env('DB_HOST'),
+        'dbname'   => env('DB_NAME'),
+        'user'     => env('DB_USER'),
+        'password' => env('DB_PASSWORD'),
+        'charset'  => env('DB_CHARSET', 'utf8mb4'),
+    ],
 ];
